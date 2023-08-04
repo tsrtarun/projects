501c0a51e1e5da9aa7cdb7f65360481bedc8d1a1
tsrtarun <tarun6467@gmail.com>
2023-08-04 23:33:27 +0530

    Weapp

--- src/utils.py
+++ src/utils.py
@@ -23,13 +23,18 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         logging.info("Evaluating models initiated")
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
@@ -44,5 +49,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
 
             return report
         
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
